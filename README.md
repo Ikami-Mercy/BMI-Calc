e5bb0666758372ff43beb75f832816db0f1b28d1
Tindi <ikami@tutanota.com>
2020-04-28 17:44:21 +0300

    first Version

---
+++ README.md
@@ -0,0 +1,5 @@
+
+# BMI Calculator
+
+            ### Screenshots
+ <img src="ScreenShots/screen1.jpeg" width="210"> <img src="ScreenShots/screen2.jpeg" width="210">
---
+++ lib/constants.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+const kBottomContainerHeight = 60.0;
+const kActiveCardColour = Color(0xFF1D1E33);
+const kInactiveCardColour = Color(0xFF111328);
+const kBottomContainerColour = Color(0xFF009688);
+
+const kLabelTextStyle = TextStyle(
+    fontSize: 18.0, color: Color(0xFF8D8E98), fontFamily: 'AvenirLTStd');
+
+const kNumberTextStyle = TextStyle(
+    fontSize: 50.0, fontWeight: FontWeight.w900, fontFamily: 'AvenirLTStd');
+
+const kLargeButtonTextStyle = TextStyle(
+    fontSize: 25.0, fontWeight: FontWeight.bold, fontFamily: 'AvenirLTStd');
+
+const kTitleTextStyle = TextStyle(
+    fontSize: 50.0, fontWeight: FontWeight.bold, fontFamily: 'AvenirLTStd');
+
+const kResultTextStyle = TextStyle(
+    color: Color(0xFF24D876),
+    fontSize: 22.0,
+    fontWeight: FontWeight.bold,
+    fontFamily: 'AvenirLTStd');
+
+const kBMITextStyle = TextStyle(
+    fontSize: 100.0, fontWeight: FontWeight.bold, fontFamily: 'AvenirLTStd');
+
+const kBodyTextStyle = TextStyle(fontSize: 22.0, fontFamily: 'AvenirLTStd');
