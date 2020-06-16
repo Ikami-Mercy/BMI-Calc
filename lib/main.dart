a07bf7b1fcb67eb9a84d36b7b1c3e219ed5e656e
Tindi <ikami@tutanota.com>
2020-06-16 16:26:16 +0300

    Set launcher icon

--- lib/main.dart
+++ lib/main.dart
@@ -1,17 +1,27 @@
+import 'package:animated_splash/animated_splash.dart';
 import 'package:flutter/material.dart';
 import 'package:bmi_calculator/screens/input_page.dart';
+import 'package:flutter_statusbarcolor/flutter_statusbarcolor.dart';
 
 void main() => runApp(BMICalculator());
 
 class BMICalculator extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
+    FlutterStatusbarcolor.setStatusBarColor(Color(0xFF0A0E21));
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData.dark().copyWith(
         primaryColor: Color(0xFF0A0E21),
         scaffoldBackgroundColor: Color(0xFF0A0E21),
       ),
-      home: InputPage(),
+      home: AnimatedSplash(
+        imagePath: 'images/logo.png',
+        home: InputPage(),
+        duration: 2500,
+        type: AnimatedSplashType.StaticDuration,
+      ),
+
     );
   }
 }
